aa78379ff4531934041a5af3b1bf081b8fe88bf6
Will Cooley <william.cooley@gmail.coom>
2023-12-01 10:43:12 -0500

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
 # your code goes here!
+import ipdb
+
+class Anagram:
+    def __init__(self, word="someword"):
+        self.word = word
+    
+    def match(self, word_list):
+        split_provided_word = [letter for letter in self.word]
+        matched_word = []
+        for word in word_list:
+            split_word = [letter for letter in word]
+            if sorted(split_provided_word) == sorted(split_word):
+                matched_word.append(word)
+        return matched_word
+            
+        
+
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+
